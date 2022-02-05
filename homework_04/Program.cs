56f18d5bd0eae87ff587c2ae8a9a8bcd27f01849
Timur <prytotype@yandex.ru>
2022-02-05 14:13:08 +0300

    Homework 4 completed

---
+++ homework_04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача № 4. Найти максимальное из трёх чисел
+
+int FindMaxNumber(int first, int second, int third)
+{
+     int max = first;
+     
+     if(second > first)
+     max = second;
+     if(third > second)
+     max = third;
+
+ return max;
+}
+
+Console.Write("Введите первое число -> ");
+string firstStr = Console.ReadLine();
+Console.Write("Введите второе число -> ");
+string secondStr = Console.ReadLine();
+Console.Write("Введите третье чилсо -> ");
+string thirdStr = Console.ReadLine();
+
+int numberOne = int.Parse(firstStr);
+int numberTwo = int.Parse(secondStr);
+int numberThree = int.Parse(thirdStr);
+
+int result = FindMaxNumber(numberOne, numberTwo, numberThree);
+Console.WriteLine($"Пользователем даны следующие числа -> {numberOne}, {numberTwo}, {numberThree}");
+Console.Write($"Наибольшим числом является: {result}");
