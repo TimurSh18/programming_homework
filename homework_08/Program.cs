952d321f87ac1d61f91613fca1fc66dff738be96
Timur <prytotype@yandex.ru>
2022-02-05 14:30:06 +0300

    Homework 8 completed

---
+++ homework_08/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8. Показать четные числа от 1 до N
+
+Console.Write("Введите число до которого вы хотите посмотреть чётные числа: ");
+string nStr = Console.ReadLine();
+
+int n = int.Parse(nStr);
+
+int even = 2;
+while(even <= n)
+{
+   Console.WriteLine(even);
+   even += 2;
+}
+ 
