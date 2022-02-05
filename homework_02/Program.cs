7e46fcef1d5663c59e32a372f055db594821721c
Timur <prytotype@yandex.ru>
2022-02-05 13:52:07 +0300

    Homework 2 completed

---
+++ homework_02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2.  Даны 2 числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число: ");
+string firstNumber = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string secondNumber = Console.ReadLine();
+
+int numberOne = int.Parse(firstNumber);
+int numberTwo = int.Parse(secondNumber);
+
+if(numberOne > numberTwo)
+{
+    Console.Write($"Наибольшее число = {numberOne}. Наименьшее число = {numberTwo}");
+}
+else
+{
+    Console.Write($"Наибольшее число = {numberTwo}. Наименьшее число = {numberOne}");
+}
