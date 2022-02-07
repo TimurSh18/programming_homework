c167574da011064fd6bda0a4b8543893207d66e6
Timur <prytotype@yandex.ru>
2022-02-07 03:43:04 +0300

    Homework 10 completed

---
+++ homework_010/Program.cs
@@ -0,0 +1,11 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано случайное число {number}");
+
+// Находим первые две цифры в числе:
+int result = number / 10;
+
+// Находим вторую цифру из имеюшихся двух:
+result = result % 10;
+Console.WriteLine(result);
