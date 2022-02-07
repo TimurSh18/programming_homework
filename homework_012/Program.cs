155bf3690e94142c814423491dafd53f5c9594a9
Timur <prytotype@yandex.ru>
2022-02-07 04:01:27 +0300

    Homework 12 completed

---
+++ homework_012/Program.cs
@@ -0,0 +1,14 @@
+﻿// // 12. Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано случайное число -> {number}");
+
+// Находим последнюю цифру числа
+int lastDigit = number % 10;
+
+//Находим первую цифру числа 
+int firstDigit = number / 100;
+
+//Выводим первоначальное число без второй цифры 
+int result = firstDigit*10 + lastDigit;
+Console.WriteLine($"Число после удаления второй цифры -> {result}");
