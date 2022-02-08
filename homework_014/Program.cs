fab6c3faaa5771a55108b957d4e43db15d25340f
Timur <prytotype@yandex.ru>
2022-02-08 19:08:36 +0300

    Homework 14 completed

--- homework_014/Program.cs
+++ homework_014/Program.cs
@@ -1,15 +1,16 @@
-﻿Console.Write("Введите число: ");
+﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+
+Console.Write("Введите число: ");
 
 string valueStr = Console.ReadLine();
-if (int.TryParse(valueStr, out int valueInt))
+if (int.TryParse(valueStr, out int valueInt)) //Проверка на число
 {
     string resultValue = valueInt.ToString(); // Число обратно в строку
    if(resultValue.Length >=3)
-   Console.WriteLine($"Третья цифры числа =   {valueStr[2]}"); // выводится 3 цифра числа [0, 1, 2]
+   Console.WriteLine($"Третья цифра числа =  {valueStr[2]}"); // выводится 3 цифра числа [0, 1, 2]
    else
-   Console.WriteLine("Число не трёхзначное");
+   Console.WriteLine("В числе отсутствует третья цифра");
 }
 
 else
-Console.WriteLine("В данном числе отсутствует третья цифра");
-//
+Console.Write("Вы ввели не число!");
