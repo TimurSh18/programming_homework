046377dddbb1b3fa2a753075b5278c32a1f751c5
Timur <prytotype@yandex.ru>
2022-02-08 19:51:10 +0300

    Homework 16 completed

---
+++ homework_016/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 16: Дано число обозначающее день недели. Выяснить является ли номер дня недели выходным.
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string dayStr = Console.ReadLine();
+    int dayInt = int.Parse(dayStr);
+
+    return dayInt;
+}
+
+string[] array = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+int day = InPut("Введите день недели от 1 до 7: ");
+
+if (day == 6 || day == 7)
+    Console.WriteLine($"Вы выбрали ***{array[day-1]}*** - выходной");
+
+else if (day < 1 || day > 7)
+    Console.WriteLine("Такого дня недели не существует");
+
+else 
+    Console.WriteLine($"Вы выбрали ***{array[day-1]}*** - рабочий день");
