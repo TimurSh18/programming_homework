ce13be5974f883317ca210baddc88d9ed3d9252d
Timur <prytotype@yandex.ru>
2022-02-09 16:46:59 +0300

    Homework 24 completed

---
+++ homework_024/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24. Найти кубы чисел от 1 до N
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string aStr = Console.ReadLine();
+    int aInt = int.Parse(aStr);
+
+    return aInt;
+}
+
+int a = InPut("Введите число до которого хотите посмотреть кубы чисел: ");
+int i = 1;
+
+while(i < a) // Цикл для выведения кубов чисел, решил сделать без массива
+{
+    Console.Write($"Куб числа {i} = ");
+    Console.WriteLine(i*i*i);
+    i++;
+}
