c8ffb2d9d933a84f3ce22eeba053718af9202a8b
Timur <prytotype@yandex.ru>
2022-02-16 14:26:16 +0300

    homework 26 completed

---
+++ homework_026/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 26: Возведите число А в натуральную степень B используя цикл
+
+int InPut(string message)
+{
+    Console.Write(message);
+    string firstNumberStr = Console.ReadLine();
+    int firstNumber = int.Parse(firstNumberStr);
+    return firstNumber;
+}
+
+void Multiplication(int numberfirst, int numbersecond)
+{
+
+    int i = 1;
+     int result = numberfirst;
+    while(i<numbersecond)
+    {
+        result = result*numberfirst;
+     
+        i++;
+      }
+      Console.Write($"Результат вычисления = {result}");  
+    }
+
+    
+
+int digit = InPut("Введите число, которое вы собираетесь возводить в степень: ");
+
+
+int number = 0;
+while( number < 1)
+{
+  Console.Write("Введите натуральную степень в которую вы хотите возвести число: ");
+  string numberStr = Console.ReadLine();
+  int.TryParse(numberStr, out number);
+}
+
+Multiplication(digit, number);
+
