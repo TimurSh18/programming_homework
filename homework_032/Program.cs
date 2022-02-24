95354fe9988808d176dd3bcb0f8750d843dd51f6
Timur <prytotype@yandex.ru>
2022-02-24 12:07:50 +0300

    homework 32 completed

---
+++ homework_032/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 32: Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] arr = new int[8];
+
+void FullArray()
+{
+   Random number = new Random();
+
+   for(int i = 0; i<arr.Length; i++)
+   {
+       arr[i] = number.Next(0,2);
+       Console.WriteLine($"{arr[i]}  ");
+   }
+}
+FullArray();
