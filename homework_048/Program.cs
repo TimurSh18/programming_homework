ef752637b5f740dc39d87be0dfb47536f973989d
Timur <prytotype@yandex.ru>
2022-04-01 15:40:54 +0300

    homework 48 completed

---
+++ homework_048/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 48: Показать двумерный массив размером m×n заполненный целыми числами.
+
+
+void PrintFillMatrix(int[,] matrFill)
+{
+    Random numberToFill = new Random();
+
+    for(int j = 0; j<matrFill.GetLength(0); j++)
+    {
+        for(int k=0; k<matrFill.GetLength(1); k++)
+        {
+        
+            matrFill[j,k] = numberToFill.Next(1,100);
+            Console.Write($"{matrFill[j,k]}  ");        
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] matrix = new int [2,5];
+PrintFillMatrix(matrix);
